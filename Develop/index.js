5b4ca4adead98b74f1b556eeafa60158c9db40b4
Trevor L <trevorlong992@gmail.com>
2022-02-06 12:50:50 -0500

    First commit of the day. Might even have it done by the end of the day. Liking backend much more than frontend

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,16 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:"input",
+        name:"title",
+        message:"Please enter a title for your Project"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
